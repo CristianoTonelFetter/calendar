8884d0581ab82eb797868d9fe38eb471b5aa39ee
Cristiano Tonel Fetter <tonel@devgrid.co.uk>
2020-01-28 14:00:24 -0300

    Calendar grid basic styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,59 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import classNames from 'classnames';
+
+import style from './App.module.scss';
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="calendar" className={style.calendar}>
+      <div className={style.header}>Sunday</div>
+      <div className={style.header}>Monday</div>
+      <div className={style.header}>Tuesday</div>
+      <div className={style.header}>Wednesday</div>
+      <div className={style.header}>Thurday</div>
+      <div className={style.header}>Friday</div>
+      <div className={style.header}>Saturday</div>
+
+      <div className={classNames(style.cell, style.disabled)}>1</div>
+      <div className={style.cell}>2</div>
+      <div className={style.cell}>3</div>
+      <div className={style.cell}>4</div>
+      <div className={style.cell}>5</div>
+      <div className={style.cell}>6</div>
+      <div className={style.cell}>7</div>
+
+      <div className={style.cell}>8</div>
+      <div className={style.cell}>9</div>
+      <div className={style.cell}>10</div>
+      <div className={style.cell}>11</div>
+      <div className={style.cell}>12</div>
+      <div className={style.cell}>13</div>
+      <div className={style.cell}>14</div>
+
+      <div className={style.cell}>15</div>
+      <div className={style.cell}>16</div>
+      <div className={style.cell}>17</div>
+      <div className={style.cell}>18</div>
+      <div className={style.cell}>19</div>
+      <div className={style.cell}>20</div>
+      <div className={style.cell}>21</div>
+
+      <div className={style.cell}>22</div>
+      <div className={style.cell}>23</div>
+      <div className={style.cell}>24</div>
+      <div className={style.cell}>25</div>
+      <div className={style.cell}>26</div>
+      <div className={style.cell}>27</div>
+      <div className={style.cell}>28</div>
+
+      <div className={style.cell}>29</div>
+      <div className={style.cell}>30</div>
+      <div className={style.cell}>31</div>
+      <div className={style.cell}>1</div>
+      <div className={style.cell}>2</div>
+      <div className={style.cell}>3</div>
+      <div className={style.cell}>4</div>
     </div>
   );
 }
